602fc477a5e28cea2a622d51edb182960073bb4a
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-08-20 12:28:55 -0500

    Added boilerplate src files

--- sauti/gulpfile.js
+++ sauti/gulpfile.js
@@ -1,6 +1,9 @@
-function defaultTask(cb) {
-    // Code for my default task goes here 
-    cb(); 
-}
+const gulp = require('gulp'); 
 
-exports.default = defaultTask
+gulp.task('process-css', function() {
+    return gulp.src('./src/index.css')
+    .pipe(gulp.dest('./src/dist'));
+});  
+
+
+gulp.task('default', ['process-css']); 
