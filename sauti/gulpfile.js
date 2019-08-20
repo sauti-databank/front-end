c63216f49a2435e5f922b6739d45822437040e61
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-08-20 16:01:36 -0500

    Added JSMinify tests

--- sauti/gulpfile.js
+++ sauti/gulpfile.js
@@ -1,14 +1,14 @@
 const gulp = require('gulp'); 
-// Adding gulp-clean-css to minify css
-const cleanCSS = require('gulp-clean-css'); 
-// Compiles sass
-const sass = require('gulp-sass'); 
-// Minify js 
-const uglify = require('gulp-uglify'); 
 
- gulp.task('default', function() {
-     console.log("Gulp js is running")
- });  
+// Minifying styling 
+const cleanCSS = require('gulp-clean-css'); 
+const sass = require('gulp-sass'); 
+
+// Minifying JS 
+const babel = require('gulp-babel'); 
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');  
+const uglify = require('gulp-uglify'); 
 
 // Copies css to publc folder 
 gulp.task('css', function () {
@@ -18,8 +18,21 @@ gulp.task('css', function () {
         .pipe(gulp.dest('public/css'));
 }) 
 
+// Minifies JS 
 gulp.task('jsMinify', function() {
     return gulp.src('src/js/App.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ["@babel/preset-react", 
+        "@babel/preset-env"], 
+        plugins: ["@babel/plugin-proposal-class-properties"]
+    }))
+    .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(gulp.dest('public/js'));
 });
+
+// Test 
+gulp.task('default', function() {
+    console.log("Gulp js is running")
+}); 
